3a6b74bd9b8dec707d3dc0da0e52281dfdef91b2
Yang <overseeas@gmail.com>
2023-12-27 11:04:25 +0900

    add article new page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,17 @@ class ArticlesController < ApplicationController
   end
 
   def index
+    @articles = Article.all
+  end
 
+  def new
+
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article
   end
 
 end
